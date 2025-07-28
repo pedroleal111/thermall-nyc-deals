93745d3508052ce1e5dd368aa8b78ebd9a42a654
Pedro Leal <pedroleal0102@gmail.com>
2025-07-28 14:16:18 -0300

    Update ProductGrid.tsx

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -60,7 +60,7 @@ const ProductGrid = () => {
       originalPrice: 55.00,
       rating: 4.5,
       reviews: 634,
-      image: "https://images.pexels.com/photos/5274533/pexels-photo-5274533.jpeg",
+      image: "https://images.unsplash.com/photo-1593546189050-2705e874df8c?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       badge: "Designer",
       colors: ["Rose Gold", "Silver", "Marble"]
     },
